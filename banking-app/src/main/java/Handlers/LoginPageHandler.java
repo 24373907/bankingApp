6178a954197c2da113cdc4032ddf676c997a6b4d
Tristan Miller <trmille022@student.wethinkcode.co.za>
2024-03-17 16:37:30 +0200

    not much progress tbh

--- banking-app/src/main/java/Handlers/LoginPageHandler.java
+++ banking-app/src/main/java/Handlers/LoginPageHandler.java
@@ -2,13 +2,19 @@ package Handlers;
 
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 public class LoginPageHandler {
+ 
+    // public static Context loginHandler(Context ctx) throws IOException {
 
+    //     ctx.result((Files.readAllBytes(Paths.get("banking-app/src/main/resources/public/login.html"))));
+    //     return ctx;
+    // }
 
-    public static Context loginHandler(Context ctx) {
-
-
-        return ctx.render("banking-app\\src\\main\\resources\\html\\index.html");
-    }
+    public static Handler loginHandler = context -> {
+        context.result((Files.readAllBytes(Paths.get("banking-app/src/main/resources/public/login.html"))));
+    };
 }
--- banking-app/src/main/java/main/webServer.java
+++ banking-app/src/main/java/main/webServer.java
@@ -1,23 +1,35 @@
 package main;
 
-import io.javalin.*;
 import Handlers.LoginPageHandler;
-import org.thymeleaf.*;
+import freemarker.template.Configuration;
+import io.javalin.*;
+import io.javalin.http.staticfiles.Location;
+import java.io.File;
+import java.io.IOException;
+
 
 public class webServer {
 
     static Javalin server;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        setupFreemakerConfig();
         startServer();
     }
 
     public static void startServer() {
 
-        server = Javalin.create(config -> {
-            config.fileRenderer(new FileRenderer());
-        })
-        .get("/", ctx -> LoginPageHandler.loginHandler(ctx))
-        .start(8000);
+        Javalin.create(config -> {
+                    config.staticFiles.add("banking-app/src/main/resources/public", Location.EXTERNAL);
+                })
+                .get("/login", LoginPageHandler.loginHandler)
+                .start(8079);
+    }
+
+    private static void setupFreemakerConfig() throws IOException {
+
+        Configuration fConfiguration = new Configuration(Configuration.VERSION_2_3_32);
+        fConfiguration.setDefaultEncoding("UTF-8");
+        fConfiguration.setDirectoryForTemplateLoading(new File("banking-app/src/main/resources/public"));
     }
 }
